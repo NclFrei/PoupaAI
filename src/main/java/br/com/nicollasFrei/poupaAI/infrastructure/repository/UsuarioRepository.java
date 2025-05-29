f353fcdff8a5d04e0a30546d78fef6a27010ab07
NclFrei <nicollasfreieleoterio@gmail.com>
2025-05-29 17:54:51 -0300

    Adicionando pasta security + model Usuario

---
+++ src/main/java/br/com/nicollasFrei/poupaAI/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package br.com.nicollasFrei.poupaAI.infrastructure.repository;
+
+import br.com.nicollasFrei.poupaAI.infrastructure.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> deleteByEmail(String email);
+}
---
+++ src/test/java/br/com/nicollasFrei/poupaAI/PoupaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nicollasFrei.poupaAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoupaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
