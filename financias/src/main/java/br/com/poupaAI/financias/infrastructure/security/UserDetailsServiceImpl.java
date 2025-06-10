7b2a70c4d2d03974f4930b660cc4374478abd8d2
NclFrei <nicollasfreieleoterio@gmail.com>
2025-06-10 00:12:12 -0300

    Adicionadas configurações de segurança

---
+++ financias/src/main/java/br/com/poupaAI/financias/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package br.com.poupaAI.financias.infrastructure.security;
+
+
+import br.com.poupaAI.financias.business.dto.UsuarioDTO;
+import br.com.poupaAI.financias.infrastructure.client.UsuarioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl {
+
+    // Repositório para acessar dados de usuário no banco de dados
+    @Autowired
+    private UsuarioClient client;
+
+
+    public UserDetails carregaDadosUsuario(String email, String token){
+        UsuarioDTO usuarioDTO = client.buscaUsuarioPorEmail(email, token);
+        return User
+                .withUsername(usuarioDTO.getEmail()) // Define o nome de usuário como o e-mail
+                .password(usuarioDTO.getSenha())// Define a senha do usuário
+                .build();
+    }
+}
