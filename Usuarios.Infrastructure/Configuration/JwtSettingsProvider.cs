a5a74dd1e1247e93912e5e3ef9f7d7a44af91773
NclFrei <nicollasfreieleoterio@gmail.com>
2025-07-22 21:19:29 -0300

    Implementando o TokenService para gerar o JWT

---
+++ Usuarios.Infrastructure/Configuration/JwtSettingsProvider.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Options;
+using Usuarios.Infrastructure.Interfaces;
+
+namespace Usuarios.Infrastructure.Configuration;
+
+public class JwtSettingsProvider : IJwtSettingsProvider
+{
+    private readonly JWTSettings _jwtSettings;
+
+    public JwtSettingsProvider(IOptions<JWTSettings> jwtSettings)
+    {
+        _jwtSettings = jwtSettings.Value;
+    }
+
+    public string SecretKey => _jwtSettings.SecretKey;
+    
+}
