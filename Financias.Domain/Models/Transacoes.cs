9eec122f0f00e2a285f056a7e080f4a848ece68b
NclFrei <nicollasfreieleoterio@gmail.com>
2025-08-01 00:39:47 -0300

    Criando microserviço de financias

---
+++ Financias.Domain/Models/Transacoes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financias.Domain.Models;
+
+public class Transacoes
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public DateTime DataTransacao { get; set; }
+    public decimal Valor { get; set; }
+    public int CategoriaId { get; set; } 
+    public Categoria Categoria { get; set; } 
+    public int UsuarioId { get; set; } 
+}
